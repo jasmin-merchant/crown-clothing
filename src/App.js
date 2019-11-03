5c04af8c911c13a9f7beba1c4644c3523d5cacc3
System Administrator <root@Jasmins-MacBook-Air.local>
2019-11-03 10:32:19 +0530

    Divided homepage into smaller components and made changes in menu items design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+
+const Hats = () => (
+  <div>
+    Hats Page
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={Hats} />
+      </Switch>
     </div>
   );
 }
