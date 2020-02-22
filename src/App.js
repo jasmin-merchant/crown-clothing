7a2e67f664fe877a78a55ac95104e775cf667356
System Administrator <root@Jasmins-MacBook-Air.local>
2020-02-22 18:18:06 +0530

    Added code to redirect to homepage if the user is signed in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignOut from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
-import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions';
 
 class App extends React.Component {
@@ -43,16 +43,20 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signIn' component={SignInSignOut} />
+          <Route exact path='/signIn' render = {() => this.props.currentUser ? (<Redirect to ='/' />) : (<SignInSignOut/>)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 }
 )
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
