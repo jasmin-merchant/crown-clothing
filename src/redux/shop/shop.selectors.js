af8921c8b07452dddded10532558dcff93ceaa44
System Administrator <root@Jasmins-MacBook-Air.local>
2020-04-26 17:14:20 +0530

    Moved Directory & Shop Data into individual reducers and created componenets for directory/shop

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,27 @@
+import { createSelector } from "reselect";
+
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  jackets: 2,
+  sneakers: 3,
+  womens: 4,
+  mens: 5,
+};
+
+const selectShop = (state) => state.shop;
+
+export const selectCollections = createSelector(
+  [selectShop],
+  (shop) => shop.collections
+);
+
+export const selectCollectiionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector([selectCollections], (collections) =>
+        collections[collectionUrlParam]
+    
+  );
